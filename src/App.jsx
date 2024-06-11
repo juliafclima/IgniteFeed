13dae12ef89dde0b05b42eab6054e1c8ac0493e8
Júlia Lima <juliafclima@hotmail.com>
2024-06-11 16:43:02 -0300

    Formulário de comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import Post from "./Post";
+import { Post } from "./components/Post";
+
 
 import styles from "./App.module.css";
 import "./global.css";
@@ -13,11 +14,8 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Diego Fernandes"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-          />
-          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
